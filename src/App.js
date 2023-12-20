4d2b46b9e54c4a924d7615dc6192d1c57a74ce75
Fatemeh Rezwani <57309847+FatemehRezwani@users.noreply.github.com>
2023-12-20 10:11:19 +0430

    import react-Icon and use bundle.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout';
@@ -10,14 +11,12 @@ import { Contact } from './pages/Contact';
 function App() {
   return (
     <div className="App">
-
       <Router>
         <Routes>
           <Route path='/' element={<Layout/>} >
             <Route index element={<Home/>}/>
             <Route path='about' element={<About/>}/>
             <Route path='contact' element={<Contact/>}/>
-
           </Route>
         </Routes>
       </Router>
