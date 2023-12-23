7e14a6db23376e6f51ead68dc05851469c9e636a
Fatemeh Rezwani <57309847+FatemehRezwani@users.noreply.github.com>
2023-12-23 12:34:05 +0430

    something changed in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = ()=>{
                     <div className="row align-items-center">
                         <div className="col-2">
                             <h2> 
-                                <Link className='text-white'>Best In Tech</Link>
+                                <Link className='text-white'><img className="img-fluid" src="images/shopify-ico.svg" alt="shopify" />Shopify</Link>
                             </h2>
                         </div>
                         <div className="col-5">
