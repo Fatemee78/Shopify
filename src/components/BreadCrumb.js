877db2afd853d537a8b66ae796989d36375cf5f9
Fatemeh Bakhshi <57309847+f-Bakhshi@users.noreply.github.com>
2024-01-04 13:01:28 +0430

    Meta and BreadCrumbs component added

---
+++ src/components/BreadCrumb.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const BreadCrumb = (props) => {
+    const {title} = props;
+    
+  return (
+    <div className='breadcrumb py-4 mb-0'>
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                   <p className='text-center mb-0'>
+                    <Link to='/' className='text-dark'>Home &nbsp;</Link>/{title}
+                   </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
